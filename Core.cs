c864f60f36ccd4b706ec5d4c4239317923124aed
JillTheSomething <ledbetterte@gmail.com>
2024-10-14 19:26:04 -0400

    Seperate Melon info into a seperate file.
    Also document Core since it's messy...

--- Core.cs
+++ Core.cs
@@ -2,8 +2,7 @@
 using ABI_RC.Core.Player;
 using UnityEngine;
 
-[assembly: MelonInfo(typeof(BigHeadMode.Core), "BigHeadMode", "1.0.0", "JillTheSomething", null)]
-[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+
 
 namespace BigHeadMode
 {
@@ -17,50 +16,59 @@ namespace BigHeadMode
         private static MelonPreferences_Entry<bool> ScaleRemote;
         private static MelonPreferences_Entry<float> Multipler;
 
-
+        /// <summary>
+        /// Set up melon prefs and icons for BTK.
+        /// </summary>
         public override void OnInitializeMelon()
         {
-            UI.CreateCategory();
-            BigHeadModePrefrenceCategory = MelonPreferences.CreateCategory("BigHeadMode");
+            BigHeadModePrefrenceCategory = MelonPreferences.CreateCategory("BigHeadMode"); // Init melonprefs.
             ScaleLocal = MelonPreferences.CreateEntry<bool>("BigHeadMode", "ScaleLocal", true, "Scale Local Player", "Scale the local player head.");
-            ScaleRemote = MelonPreferences.CreateEntry<bool>("BigHeadMode", "ScaleRemote", true, "Scale Remote Player", "Scale remote players head.");
+            ScaleRemote = MelonPreferences.CreateEntry<bool>("BigHeadMode", "ScaleRemote", true, "Scale Remote Player", "Scale remote player's heads.");
             Multipler = MelonPreferences.CreateEntry<float>("BigHeadMode", "Multiplier", 2.0f, "Multiplier", "What to grow/shrink the head by (shrinks using `headScale * 1/multiplier`.");
 
-            UI.LoadIcons(MelonAssembly.Assembly);
-            LoggerInstance.Msg("BigHeadMode Initialized.");
+            UI.LoadIcons(MelonAssembly.Assembly); // Init BTK buttons.
+            UI.CreateCategory();  
+            //LoggerInstance.Msg("BigHeadMode Initialized.");
         }
 
+        /// <summary>
+        /// Scale heads the user requested but were not loaded at the time.
+        /// </summary>
         public override void OnUpdate()
         {
             base.OnUpdate();
-            if (LateScaleQueue.Count > 0)
+            if (LateScaleQueue.Count > 0)  // Only process if we have to.
             {
 
-                List<string> toClear = new List<string>();
+                List<string> toClear = new List<string>();  // What entries we can remove.
                 foreach (string uuid in LateScaleQueue.Keys)
                 {
                     if (LateScaleQueue[uuid].Item1.transform != null && LateScaleQueue[uuid].Item1.transform.GetChild(2).childCount != 0)
                     {
                         //MelonLogger.Msg($"Late Scaling uuid {uuid}");
-                        GameObject avatar = LateScaleQueue[uuid].Item1.transform.GetChild(2).GetChild(0).gameObject;
+                        GameObject avatar = LateScaleQueue[uuid].Item1.transform.GetChild(2).GetChild(0).gameObject;  // Get and scale the bone now.
                         Animator animator = avatar.GetComponent<Animator>();
                         Transform headBone = animator.GetBoneTransform(HumanBodyBones.Head);
                         SetHeadSize(headBone, uuid, LateScaleQueue[uuid].Item2);
-                        toClear.Add(uuid);
+                        toClear.Add(uuid);  // Prepare to remove from LateScaleQueue.
                     }
                 }
-                foreach (string uuid in toClear) LateScaleQueue.Remove(uuid);
+                foreach (string uuid in toClear) LateScaleQueue.Remove(uuid);  // Clear LateScaleQueue where possible.
             }
         }
 
+        /// <summary>
+        /// Get the list of users in the current instance as CVRPlayerEntities.
+        /// </summary>
+        /// <returns>CVRPlayerEntites of users in current instance.</returns>
         public static List<CVRPlayerEntity> GetUserlist()
         {
             List<CVRPlayerEntity> playerList = new List<CVRPlayerEntity>();
-            if (!(ScaleLocal.Value || ScaleRemote.Value))
+            if (!(ScaleLocal.Value || ScaleRemote.Value))  // If user requests nothing to happen, tell them that.
             {
                 MelonLogger.Warning("ScaleLocal and ScaleRemote are both set to false. Set at least one to true if you want to grow/shrink any heads!");
             }
-            if (ScaleRemote.Value)
+            if (ScaleRemote.Value)  // Get remote users if asked.
             {
                 foreach (CVRPlayerEntity p in CVRPlayerManager.Instance.NetworkPlayers)
                 {
@@ -70,19 +78,24 @@ namespace BigHeadMode
             return playerList;
         }
 
+        /// <summary>
+        /// Scale all heads or queue heads to scale.
+        /// </summary>
+        /// <param name="players">List of players to scale.</param>
+        /// <param name="multiplier">Value to multiply/divide head scale by.</param>
         public static void SetAllHeadSize(List<CVRPlayerEntity> players, float multiplier)
         {
-            LateScaleQueue.Clear();
+            LateScaleQueue.Clear();  // New request from user, forget old and unfufilled requests.
             foreach (CVRPlayerEntity p in players)
             {
-                try
+                try  // Because I'd rather fail quietly.
                 {
                     //MelonLogger.Msg($"Scaling {p.Username} head by {multiplier}.");
-                    if (p.PlayerObject.transform.GetChild(2).childCount != 0) { 
-                        GameObject avatar = p.PlayerObject.transform.GetChild(2).GetChild(0).gameObject;
+                    if (p.PlayerObject.transform.GetChild(2).childCount != 0) {  // Dont try to scale if avatar is not loaded, queue it up for when it is.
+                        GameObject avatar = p.PlayerObject.transform.GetChild(2).GetChild(0).gameObject;  // Get the head bone (in several steps).
                         Animator animator = avatar.GetComponent<Animator>();
                         Transform headBone = animator.GetBoneTransform(HumanBodyBones.Head);
-                        SetHeadSize(headBone, p.Uuid, multiplier);
+                        SetHeadSize(headBone, p.Uuid, multiplier); // Scale the head bone.
                     }
                     else
                     {
@@ -94,24 +107,29 @@ namespace BigHeadMode
                     MelonLogger.Error($"  Could not scale:\n  {e}");
                 }
             }
-            if (ScaleLocal.Value)
+            if (ScaleLocal.Value) // Scale the local user's head, if the want.
             {
                 //MelonLogger.Msg($"Scaling local user head by {multiplier}.");
                 Transform headBone = PlayerSetup.Instance.animatorManager.Animator.GetBoneTransform(HumanBodyBones.Head);
-                SetHeadSize(headBone, "local", multiplier);
+                SetHeadSize(headBone, "local", multiplier);  // Local user saves the origial scale with the key "local".
             }
         }
-
+        /// <summary>
+        /// Scale a transform and store the origial scale in OriginalHeadScales.
+        /// </summary>
+        /// <param name="headBone">HeadBone to scale.</param>
+        /// <param name="uuid">UUID of the user who is being scaled.</param>
+        /// <param name="multiplier">Value to scale by.</param>
         public static void SetHeadSize(Transform headBone, string uuid, float multiplier)
         {
-            try
+            try  // I'd rather fail silently.
             {
                 Vector3 originalScale;
-                if (OriginalHeadScales.ContainsKey(uuid)) originalScale = OriginalHeadScales[uuid];
+                if (OriginalHeadScales.ContainsKey(uuid)) originalScale = OriginalHeadScales[uuid];  // Use the original scale if we have one stored.
                 else
                 {
                     originalScale = headBone.localScale;
-                    OriginalHeadScales.Add(uuid, headBone.localScale);
+                    OriginalHeadScales.Add(uuid, headBone.localScale);  // Save the original head scale so we can revert/rescale later...
                 }
                 headBone.localScale = new Vector3(originalScale.x * multiplier, originalScale.y * multiplier, originalScale.z * multiplier);
             }
@@ -121,54 +139,71 @@ namespace BigHeadMode
             }
         }
 
+        /// <summary>
+        /// Resore heads to original size using OriginalHeadScales.
+        /// </summary>
         public static void RestoreHeadSize()
         {
-            List<string> toClear = new List<string>();
-            LateScaleQueue.Clear();
+            List<string> toClear = new List<string>();  // Heads that have successfully been restored.
+            LateScaleQueue.Clear();  // If we had something we were going to scale, now we dont.
             foreach (string k in OriginalHeadScales.Keys)
             {
-                if(k == "local")
+                if(k == "local")  // Handle restoring the local user.
                 {
                     Transform headBone = PlayerSetup.Instance.animatorManager.Animator.GetBoneTransform(HumanBodyBones.Head);
                     headBone.localScale = OriginalHeadScales["local"];
                     toClear.Add("local");
                     continue;
                 }
-                try
+                
+                try // Handle the remote user (since they aren't local).
                 {
                     CVRPlayerEntity p = null;
-                    foreach (CVRPlayerEntity pe in CVRPlayerManager.Instance.NetworkPlayers)
+                    foreach (CVRPlayerEntity pe in CVRPlayerManager.Instance.NetworkPlayers) // Search through the player list for our user, I don't trust CVRPlayerEntity to not explode on me later so I'm storing the UUID instead.
                     {
                         if (pe.Uuid == k && pe.PlayerObject != null) p = pe;
                     }
-                    if (p == null) continue;
-                    toClear.Add(k);
+                    if (p == null) // Could not find the player to restore, they probably left...
+                    {
+                        toClear.Add(k);
+                        continue;
+                    }  
                     //MelonLogger.Msg($"Restoring {p.Username} head.");
-                    GameObject avatar = p.PlayerObject.transform.GetChild(2).GetChild(0).gameObject;
+                    GameObject avatar = p.PlayerObject.transform.GetChild(2).GetChild(0).gameObject; // Fix they head.
                     Animator animator = avatar.GetComponent<Animator>();
                     Transform headBone = animator.GetBoneTransform(HumanBodyBones.Head);
                     headBone.localScale = OriginalHeadScales[k];
+                    toClear.Add(k);  // We fixed them!, we can remove them now...
                 }
-                catch //(Exception e)
+                catch //(Exception e)  // If something goes wrong dont mention it.
                 {
                     //MelonLogger.Warning($"RestoreHeadSize failed:\n{e}");
                 }
             }
-            foreach (string k in toClear) OriginalHeadScales.Remove(k);
+            foreach (string k in toClear) OriginalHeadScales.Remove(k);  // Clear orignal scales since we're back to original.
         }
 
+        /// <summary>
+        /// Scale heads up, for BTK.
+        /// </summary>
         public static void BigHead()
         {
             List<CVRPlayerEntity> pl = GetUserlist();
             SetAllHeadSize(pl, Multipler.Value);
         }
 
+        /// <summary>
+        /// Scale heads down, for BTK.
+        /// </summary>
         public static void SmallHead()
         {
             List<CVRPlayerEntity> pl = GetUserlist();
             SetAllHeadSize(pl, 1f/Multipler.Value);
         }
 
+        /// <summary>
+        /// Scale heads to zero, for BTK.
+        /// </summary>
         public static void NoHead()
         {
             List<CVRPlayerEntity> pl = GetUserlist();
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(BigHeadMode.Core), "BigHeadMode", "1.0.0", "JillTheSomething", "https://github.com/jll123567/BigHeadMode/releases")]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonAdditionalDependencies("BTKUILib")]
+[assembly: MelonColor(255, 50, 255, 50)]
+[assembly: MelonAuthorColor(255, 0, 255, 0)]
